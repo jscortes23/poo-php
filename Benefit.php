e8a2a037de47a00651083ac4e0c04ed20dc283e8
Joan <js.199911@gmail.com>
2023-05-22 14:52:54 -0500

    Create project

---
+++ Benefit.php
@@ -0,0 +1,14 @@
+<?php
+
+class Benefit
+{
+    public $name;
+    public $descriptionShort;
+    public $descriptionLong;
+    public $link;
+    public $imgLogo;
+    public $imgBackground;
+    public $city;
+}
+
+?>
---
+++ City.php
@@ -0,0 +1,8 @@
+<?php
+
+class City
+{
+    public $name;
+}
+
+?>
